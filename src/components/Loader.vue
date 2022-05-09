41ef4f088d27c3c342671e01bd9607be91078e72
Brendan Elton <brendan.elton@gmail.com>
2022-05-09 18:02:41 +1000

    Build: Loader

--- src/components/Loader.vue
+++ src/components/Loader.vue
@@ -1,7 +1,5 @@
 <template>
   <div class="w-full h-full flex items-center justify-center">
-    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 251 251" class="text-black w-8 animate-spin">
-      <path stroke="currentColor" stroke-width="25" d="M225.102 125.551a100.001 100.001 0 00-170.71-70.71 100 100 0 0070.71 170.71" />
-    </svg>
+    <svg width="38" height="38" xmlns="http://www.w3.org/2000/svg"><defs><linearGradient x1="8.042%" y1="0%" x2="65.682%" y2="23.865%" id="prefix__a"><stop stop-opacity="0" offset="0%"/><stop stop-opacity=".631" offset="63.146%"/><stop offset="100%"/></linearGradient></defs><g transform="translate(1 1)" fill="none" fill-rule="evenodd"><path d="M36 18c0-9.94-8.06-18-18-18" stroke="url(#prefix__a)" stroke-width="2"><animateTransform attributeName="transform" type="rotate" from="0 18 18" to="360 18 18" dur="0.9s" repeatCount="indefinite"/></path><circle fill="#000" cx="36" cy="18" r="1"><animateTransform attributeName="transform" type="rotate" from="0 18 18" to="360 18 18" dur="0.9s" repeatCount="indefinite"/></circle></g></svg>
   </div>
 </template>
